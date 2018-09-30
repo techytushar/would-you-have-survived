2e52582b6335b4f1fae56ea6fc65d529a535324b
Tushar Mittal <chiragmittal.mittal@gmail.com>
2018-09-30 20:33:51 +0530

    Add github ribbon
    Signed-off-by: Tushar Mittal <chiragmittal.mittal@gmail.com>

--- app.py
+++ app.py
@@ -19,16 +19,6 @@ class Predict(Resource):
             model = pickle.load(f)
         arr = np.array([[float(data['pclass']), float(data['sex']), float(data['age']), float(data['sibsp']), float(data['parch']),0.14, float(data['port'])]])
         answer = model.predict(arr)[0]
-        print(answer)
         return {'survived': str(answer)}
 
 api.add_resource(Predict, '/predict')
-
-"""
-@app.route('/predict', methods=['POST'])
-def predict():
-    print(request.json)
-    #with open('model','rb') as f:
-    #    model = pickle.load(model, f)
-    return "Hello World"
-"""
